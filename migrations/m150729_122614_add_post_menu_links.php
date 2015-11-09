b86eaa8be568de9bee0537fc8f1520f4aaf783d1
yeesoft <sto712@gmail.com>
2015-11-09 21:55:23 +0200

    multilingual update

--- migrations/m150729_122614_add_post_menu_links.php
+++ migrations/m150729_122614_add_post_menu_links.php
@@ -8,7 +8,8 @@ class m150729_122614_add_post_menu_links extends Migration
 
     public function up()
     {
-        $this->insert('menu_link', ['id' => 'post', 'menu_id' => 'admin-main-menu', 'link' => '/post/default/index', 'label' => 'Posts', 'image' => 'pencil', 'order' => 3]);
+        $this->insert('menu_link', ['id' => 'post', 'menu_id' => 'admin-main-menu', 'link' => '/post/default/index', 'image' => 'pencil', 'order' => 3]);
+        $this->insert('menu_link_lang', ['link_id' => 'post', 'label' => 'Posts', 'language' => 'en' ]);
     }
 
     public function down()
