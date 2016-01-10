91d416b6a6c48c48054f801d3605afc3a195af4b
YeeSoft <makitrataras@gmail.com>
2016-01-10 21:17:41 +0200

    update 0.0.1-rc

--- migrations/m150729_122614_add_post_menu_links.php
+++ migrations/m150729_122614_add_post_menu_links.php
@@ -8,7 +8,7 @@ class m150729_122614_add_post_menu_links extends Migration
     public function up()
     {
         $this->insert('menu_link', ['id' => 'post', 'menu_id' => 'admin-menu', 'link' => '/post/default/index', 'image' => 'pencil', 'created_by' => 1, 'order' => 3]);
-        $this->insert('menu_link_lang', ['link_id' => 'post', 'label' => 'Posts', 'language' => 'en']);
+        $this->insert('menu_link_lang', ['link_id' => 'post', 'label' => 'Posts', 'language' => 'en-US']);
     }
 
     public function down()
